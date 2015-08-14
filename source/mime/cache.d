ef04c6e52cc45c0c53b80217426b99f6f3837635
FreeSlave <freeslave93@gmail.com>
2015-08-14 21:50:55 +0300

    Add license

--- source/mime/cache.d
+++ source/mime/cache.d
@@ -1,3 +1,13 @@
+/**
+ * Class for reading mime.cache files.
+ * Authors: 
+ *  $(LINK2 https://github.com/MyLittleRobo, Roman Chistokhodov)
+ * License: 
+ *  $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * Copyright:
+ *  Roman Chistokhodov 2015
+ */
+
 module mime.cache;
 
 import mime.common;
--- source/mime/common.d
+++ source/mime/common.d
@@ -2,6 +2,13 @@ module mime.common;
 
 package {
     static if( __VERSION__ < 2066 ) enum nogc = 1;
+    
+    static if (is(typeof({import std.string : fromStringz;}))) {
+        import std.string : fromStringz;
+    } else { //own fromStringz implementation for compatibility reasons
+        import std.c.string : strlen;
+        @system pure inout(char)[] fromStringz(inout(char)* cString) {
+            return cString ? cString[0..strlen(cString)] : null;
+        }
+    }
 }
-
-
